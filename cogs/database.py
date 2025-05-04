98bab35afff54ec31e5c9cf47a78206081a9842a
cmckee786 <mckee_christian@yahoo.com>
2025-05-04 19:21:42 -0400

    fix(?): add discord property to decorator
    fix/feat: slash database commands integrated

--- cogs/database.py
+++ cogs/database.py
@@ -1,30 +1,32 @@
 import logging, discord, os
 import sqlite3 as db
 from discord.ext import commands
+from discord import app_commands
 
 
 class Database(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='test-db-insert')
-    async def test_insert(self, ctx):
+    # These commands can potentially take up to an hour to register in discord when using '/' commands
+    @app_commands.command(name="test-db-insert", description="test inserting data values, inserts or returns db values")
+    async def test_insert(self, interaction: discord.Interaction):
         logging.info('test-db-insert cog invoked')
         try:
             with db.connect('user.db') as c:
                 check = c.execute(
-                    'SELECT id FROM user WHERE id = ?', (ctx.author.id,)).fetchone()
+                    'SELECT id FROM user WHERE id = ?', (interaction.user.id,)).fetchone()
                 if check is None:
                     c.execute('INSERT INTO user(id, username) VALUES (?,?)',
-                              (ctx.author.id,
-                               ctx.author.name,)
+                              (interaction.user.id,
+                               interaction.user.name,)
                     )
                     c.commit()
-                    logging.info(f'Name:{ctx.author.name}, ID: {ctx.author.id} inserted into user.db')
-                    await ctx.send(f'DB: {ctx.author.name} inserted into database.')
+                    logging.info(f'Name:{interaction.user.name}, ID: {interaction.user.id} inserted into user.db')
+                    await interaction.response.send_message(f'DB: {interaction.user.name} inserted into database.')
                 else:
-                    logging.info(f'Name:{ctx.author.name}, ID: {ctx.author.id} already exist within user.db')
-                    await ctx.send(f'DB: {ctx.author.name} already exists within the database.')
+                    logging.info(f'Name:{interaction.user.name}, ID: {interaction.user.id} already exist within user.db')
+                    await interaction.response.send_message(f'DB: {interaction.user.name} already exists within the database.')
         except db.Error as e:
             print(e)
             logging.error(e)
@@ -35,17 +37,17 @@ class Database(commands.Cog):
             print(e)
             logging.error(e)
 
-    @commands.command(name='test-return')
-    async def test_return(self, ctx):
+    @app_commands.command(name="test-return", description="attempts to return db values")
+    async def test_return(self, interaction: discord.Interaction):
         with db.connect('user.db') as c:
             check = c.execute(
-                'SELECT id FROM user WHERE id = ?', (ctx.author.id,)).fetchone()
+                'SELECT id FROM user WHERE id = ?', (interaction.user.id,)).fetchone()
             if check is not None:
-                logging.info(f'Name:{ctx.author.name}, ID: {ctx.author.id} found in user.db')
-                await ctx.send(f'DB: {ctx.author.name} found in user.db.')
+                logging.info(f'Name:{interaction.user.name}, ID: {interaction.user.id} found in user.db')
+                await interaction.response.send_message(f'DB: {interaction.user.name} found in user.db.')
             else:
-                logging.info(f'Name:{ctx.author.name}, ID: {ctx.author.id} absent from user.db')
-                await ctx.send(f'DB: {ctx.author.name} absent from user.db.')
+                logging.info(f'Name:{interaction.user.name}, ID: {interaction.user.id} absent from user.db')
+                await interaction.response.send_message(f'DB: {interaction.user.name} absent from user.db.')
 
 async def setup(bot):
     try:
